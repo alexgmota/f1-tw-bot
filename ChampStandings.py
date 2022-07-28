c9248be63462779179ce3f2c60039c032bd319bf
alexgmota11 <alexgmota11@gmail.com>
2022-07-28 18:56:53 +0200

    Make constructors standings image added

--- ChampStandings.py
+++ ChampStandings.py
@@ -1,18 +1,43 @@
 import requests
 import json
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
-# http://ergast.com/api/f1/current/constructorStandings
 
 def getDriversStandings():
     res = requests.get(f'http://ergast.com/api/f1/current/driverStandings.json')
     response = json.loads(res.text)
-    return response["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
+    standings = []
+    for i in response["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]:
+        standings.append((i['position'], i['points'], i['Driver']['givenName'] + " " + i['Driver']['familyName']))
+    return standings
+
+
 
 def getConstructorStandings():
     res = requests.get(f'http://ergast.com/api/f1/current/constructorStandings.json')
     response = json.loads(res.text)
-    return response["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"]
+    standings = []
+    for i in response["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"]:
+        standings.append((i['position'], i['points'], i['Constructor']['name']))
+    return standings
+
+
+def makeConstStandImg(standings):
+    img = Image.open('./templates/constructorsStandings.jpg')
+    drawer = ImageDraw.Draw(img)
+    fontNames = ImageFont.truetype('./fonts/Roboto_Slab/static/RobotoSlab-Medium.ttf', 72)
+    fontPoints = ImageFont.truetype('./fonts/Roboto_Slab/static/RobotoSlab-Regular.ttf', 72)
+    offset = 0
+    for i in standings:
+        drawer.text((380, 353 + offset), i[2], font=fontNames)
+        drawer.text((1260, 353 + offset), "{:3d}".format(int(i[1])), font=fontPoints)
+        offset += 171
+
+    img.show()
+
 
 if __name__ == '__main__':
-    print(json.dumps(getConstructorStandings(), indent=2))
-    print(json.dumps(getDriversStandings(), indent=2))
+    makeConstStandImg(getConstructorStandings())
+    # print(getDriversStandings())
