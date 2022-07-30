f362c877b2d05a6afa3187d76915412683839b50
alexgmota11 <alexgmota11@gmail.com>
2022-07-30 19:31:08 +0200

    Created Qualy lap time graph

---
+++ QualyResults.py
@@ -0,0 +1,72 @@
+from locale import normalize
+import requests
+import json 
+import numpy as np
+import matplotlib.pyplot as plt
+
+from DriverLapsComparator import strToSec
+
+def getQualyLapTimes():
+    res = requests.get('https://ergast.com/api/f1/current/last/qualifying.json')
+    response = json.loads(res.text)
+    lapTimes = []
+    for i in response["MRData"]["RaceTable"]["Races"][0]['QualifyingResults']:
+        lapTimes.append((i['Driver']['code'], getHigherQualyTime(i)))
+    lapTimes.sort(key=comparator)
+    return lapTimes
+
+
+def getHigherQualyTime(qualyResult):
+    try:
+        time = getQ3Time(qualyResult)
+    except:
+        time = getLowerQualyTime(qualyResult)
+    return strToSec(time)
+def comparator(x):
+    return x[1]
+
+def getLowerQualyTime(qualyResult):
+    try: 
+        return getQ2Time(qualyResult)
+    except:
+        return getQ1Time(qualyResult)
+
+def getQ3Time(qualyResult):
+    return qualyResult['Q3']
+
+def getQ2Time(qualyResult):
+    return qualyResult['Q2']
+
+def getQ1Time(qualyResult):
+    return qualyResult['Q1']
+
+def makeQualyGraph():
+    times = getQualyLapTimes()
+    drivers = list(map(getDrivers, times))
+    normalizedTimes = normalizeQualyTimes(list(map(getTimes, times)))
+
+    plt.style.use('./templates/mystyle.mplstyle')
+    plt.figure(figsize=[10, 5])
+    plt.title(f'Qualy Reslts')
+    plt.xlabel('Leader gap in %')
+    
+    plt.barh(drivers[::-1], normalizedTimes[::-1])
+
+    plt.savefig('./images/qualyTimes.png', dpi=300)
+    plt.close()
+
+
+def getDrivers(i):
+    return i[0]
+
+def getTimes(i):
+    return i[1]
+
+def normalizeQualyTimes(times):
+    normalizedTimes = [0]
+    for i in times[1:]:
+        normalizedTimes.append((i/times[0] - 1) * 100)
+    return normalizedTimes
+
+if __name__ == '__main__':
+    makeQualyGraph()
