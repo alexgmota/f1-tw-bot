ac5231ed5b1488b23f868b37a0565c475cef9d37
alexgmota11 <alexgmota11@gmail.com>
2022-08-18 17:18:54 +0200

    Added list of drivers id

--- DriverInfo.py
+++ DriverInfo.py
@@ -1,45 +1,77 @@
 import json
 
+
 def getData():
-    with open('utils.json') as file:
+    with open("utils.json") as file:
         return json.load(file)
+
 
 def getDriversCode(arr):
     data = getData()
     res = []
     for driver in arr:
-        res.append(data[driver]['code'])
+        res.append(data[driver]["code"])
     return res
+
 
 def getDriverCode(driverId):
     data = getData()
-    return data[driverId]['code']
+    return data[driverId]["code"]
+
 
 def getDriversTeamColor(arr):
     data = getData()
     res = []
     for driver in arr:
-        res.append(data[driver]['teamColor'])
+        res.append(data[driver]["teamColor"])
     return res
+
 
 def getDriverColor(driverId):
     data = getData()
-    return data[driverId]['color']
+    return data[driverId]["color"]
+
 
 def getDriverFamilyName(driverId):
     data = getData()
-    return data[driverId]['familyName']
+    return data[driverId]["familyName"]
+
 
 def getTeamMates():
     return [
-        ('leclerc', 'sainz'),
-        ('max_verstappen', 'perez'),
-        ('hamilton', 'russell'),
-        ('alonso', 'ocon'),
-        ('norris', 'ricciardo'),
-        ('bottas', 'zhou'),
-        ('gasly', 'tsunoda'),
-        ('mick_schumacher', 'kevin_magnussen'),
-        ('vettel', 'stroll'),
-        ('albon', 'latifi')
+        ("leclerc", "sainz"),
+        ("max_verstappen", "perez"),
+        ("hamilton", "russell"),
+        ("alonso", "ocon"),
+        ("norris", "ricciardo"),
+        ("bottas", "zhou"),
+        ("gasly", "tsunoda"),
+        ("mick_schumacher", "kevin_magnussen"),
+        ("vettel", "stroll"),
+        ("albon", "latifi"),
+    ]
+
+
+def getDrivers():
+    return [
+        "leclerc",
+        "sainz",
+        "max_verstappen",
+        "perez",
+        "hamilton",
+        "russell",
+        "alonso",
+        "ocon",
+        "norris",
+        "ricciardo",
+        "bottas",
+        "zhou",
+        "gasly",
+        "tsunoda",
+        "mick_schumacher",
+        "kevin_magnussen",
+        "vettel",
+        "stroll",
+        "albon",
+        "latifi",
     ]
