de33aa142740d281289166238a2501af49e8ace6
agmota11 <agmota@alumnos.upm.es>
2022-08-28 01:59:20 +0200

    Fixed credentials bug

--- twitterManager.py
+++ twitterManager.py
@@ -30,8 +30,8 @@ def getUploadConnection():
 def getCredentials():
     try: 
         return {
-            "acces_token": os.environ['ACCES_TOKEN'],
-            "acces_token_secret": os.environ['ACCES_TOKEN_SECRET'],
+            "acces_token": os.environ['ACCESS_TOKEN'],
+            "acces_token_secret": os.environ['ACCESS_TOKEN_SECRET'],
             "key": os.environ['KEY'],
             "key_secret": os.environ['KEY_SECRET']
         } 
